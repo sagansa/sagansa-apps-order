daaa1edf90347d5fc489e551aa4e662f0d271826
sagansa <83310652+sagansa@users.noreply.github.com>
2026-05-10 06:58:01 +0700

    update darkmode dan storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => env('FILESYSTEM_PUBLIC_ROOT', storage_path('app/public')),
+            'url' => env('FILESYSTEM_PUBLIC_URL', env('APP_URL') . '/storage'),
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
